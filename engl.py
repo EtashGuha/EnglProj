42a92fffda8904f0f3a6e4e0104ce4483575bfd6
Etash Guha <etashguha@lawn-128-61-126-42.lawn.gatech.edu>
2019-03-31 13:05:03 -0400

    sentiment working

--- engl.py
+++ engl.py
@@ -1,12 +1,14 @@
+from googlenlp import analyze
+
 logfile = open("MobyDick.txt", "r") 
-wordcount=0
+line_number = 0
 my_word = "Ishmael"
 numnames = dict()
-numnames["Ishmael"] = 0
-numnames["Nantucket"] = 0
+numnames["Ishmael"] = dict()
+numnames["Nantucket"] = dict()
 for line in logfile:
+	line_number = line_number + 1
 	for name in numnames.keys():
 	    if name in line.split():
-	        numnames[name] = numnames[name] + 1
-
-print numnames
+	    	numnames[name][line_number] = analyze(line)
+print(numnames)
